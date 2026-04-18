e657555e068b4b886cfd0e690f09bdc46e36cd02
OukaXy2 <mywisnu238@gmail.com>
2026-04-18 09:03:29 +0700

    Update sw.js

--- kronik/sw.js
+++ kronik/sw.js
@@ -2,7 +2,7 @@
 // Kronik — Service Worker
 // ============================================
 
-const CACHE_NAME = 'kronik-v3';
+const CACHE_NAME = 'kronik-v4';
 const ASSETS = [
   './index.html',
   './manifest.json',
