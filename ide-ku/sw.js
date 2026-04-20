44acfac6aed35fb0241c2573f0459f82497dc494
OukaXy2 <mywisnu238@gmail.com>
2026-04-20 15:50:53 +0700

    Update sw.js

--- ide-ku/sw.js
+++ ide-ku/sw.js
@@ -4,7 +4,8 @@ const ASSETS = [
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "/shared/ecosystem-db.js"
 ];
 
 self.addEventListener("install", e => {
